a747387c84ac4cc62072b9873af7caf5d8d1560f
pBialek99 <pawel99bialek@gmail.com>
2024-11-13 12:19:29 +0100

    Update CrawlerThread.java

--- CrawlerThread.java
+++ CrawlerThread.java
@@ -1,22 +1,20 @@
 package net.webcrawler;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.Callable;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class CrawlerThread implements Callable<Void> {
     
     private final String url;
-    private final DBConn conn;
+    private final DBConn db;
 
-    public CrawlerThread(String url, DBConn conn) {
+    public CrawlerThread(String url, DBConn db) {
         
         this.url = url;
-        this.conn = conn;
+        this.db = db;
     }
 
     @Override
@@ -26,9 +24,9 @@ public class CrawlerThread implements Callable<Void> {
             Document doc = Jsoup.connect(url).get();
             Elements urls = doc.select("a[href]");
 
-            urls.forEach(u -> conn.insertRow(u.absUrl("href"), 0));
+            urls.forEach(u -> db.insertRow(u.absUrl("href"), 0));
 
-            conn.insertRow(url, 1);
+            db.insertRow(url, 1);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
