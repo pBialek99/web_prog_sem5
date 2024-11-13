0b9c911a27904a3a43b2d722825bacf3436b4cee
pBialek99 <pawel99bialek@gmail.com>
2024-11-13 12:26:51 +0100

    Update CrawlerThread.java

--- CrawlerThread.java
+++ CrawlerThread.java
@@ -7,25 +7,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class CrawlerThread implements Callable<Void> {
-    
     private final String url;
     private final DBConn db;
 
     public CrawlerThread(String url, DBConn db) {
-        
         this.url = url;
         this.db = db;
     }
 
     @Override
     public Void call() {
-        
         try {
             Document doc = Jsoup.connect(url).get();
-            Elements urls = doc.select("a[href]");
-
+            Elements urls = doc.select("a[href]")
             urls.forEach(u -> db.insertRow(u.absUrl("href"), 0));
-
             db.insertRow(url, 1);
         } catch (IOException e) {
             System.out.println(e.getMessage());
