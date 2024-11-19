ae569ba5b0436dca30cfd2c70fe02b48c2800d36
pBialek99 <pawel99bialek@gmail.com>
2024-11-19 19:08:50 +0100

    Update WebCrawler.java

--- WebCrawler.java
+++ WebCrawler.java
@@ -7,37 +7,37 @@ import java.util.concurrent.*;
 public class WebCrawler {
     private DBConn db;
     private ExecutorService executor;
-    private int MAX_DEPTH;
+    private int maxDepth;
 
     public WebCrawler(int threads, int MAX_DEPTH) {
         this.db = new DBConn();
         this.executor = Executors.newFixedThreadPool(threads);
-        this.MAX_DEPTH
+        this.maxDepth = maxDepth;
     }
 
     private String getNext() {
         String sql = "SELECT url FROM urls WHERE seen = 0 LIMIT 1";
-        
+
         try (Statement stmt = db.connect().createStatement();
              ResultSet r = stmt.executeQuery(sql)) {
-            
+
             if (r.next()) {
                 return r.getString("url");
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        
+
         return null;
     }
 
-    private void crawl(int depth, String url, List<Future<Void>> tasks) {
-        if (depth > MAX_DEPTH) return;
+    public void crawl(int depth, String url, List<Future<Void>> tasks) {
+        if (depth > maxDepth) return;
 
-        CrawlerThread task = new CrawlerThread(url, db, depth + 1, MAX_DEPTH);
-        tasks.add.(executor.submit(task));   
+        CrawlerThread task = new CrawlerThread(url, db, depth + 1, maxDepth);
+        tasks.add(executor.submit(task));
     }
-    
+
     private void finishCrawler(List<Future<Void>> tasks) {
         for (Future<Void> t : tasks) {
             try {
@@ -50,15 +50,15 @@ public class WebCrawler {
         executor.shutdown();
         db.disconnect();
     }
-    
+
     private void startCrawler(String start) {
         db.connect();
         db.createTable();
         db.insertRow(start, 0);
-        
+
         List<Future<Void>> tasks = new ArrayList<>();
 
-        crawl(1, start, tasks)
+        crawl(1, start, tasks);
 
         finishCrawler(tasks);
     }
